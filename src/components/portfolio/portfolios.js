fd1a225742e8527c84c27eca5f6bbe959ea7bef7
guicastrofer <42876310+guicastrofer@users.noreply.github.com>
2022-04-02 09:02:07 -0300

    fix portfolios.js

--- src/components/portfolio/portfolios.js
+++ src/components/portfolio/portfolios.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-    const Portfolio = () => {
+    const Portfolios = ({ portfolios }) => {
       return (
         <div>
           <left><h15>Portfolio List</h15></left>
-          {portfolio => (
+          {portfolios.map((portfolios) => (
             <div class="card">
               <div class="card-body">
-                <h10 class="card-title">{portfolio.description}</h10>
-                <h10 class="card-subtitle">{portfolio.experienceSummary}</h10>
-                <p class="card-text">{portfolio.lastNames}</p>
+                <h10 class="card-title">{portfolios.description}</h10>
+                <h10 class="card-subtitle">{portfolios.experienceSummary}</h10>
+                <p class="card-text">{portfolios.lastNames}</p>
               </div>
             </div>
-          )}
+          ))}
         </div>
       )
     };
 
-    export default Portfolio
+    export default Portfolios
